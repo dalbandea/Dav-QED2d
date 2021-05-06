3354442bec74efd6948b8c701a9c8130e799ee21
Alberto Ramos <alberto.ramos@ific.uv.es>
2021-05-06 17:19:03 +0200

    Added some support for fermions

--- src/QED2d.jl
+++ src/QED2d.jl
@@ -22,4 +22,10 @@ export HMC!, Hamiltonian, Action, Plaquette, Qtop, OMF4!
 include("QED2dFields.jl")
 export unfold_fld!, unfold_reflect!
 
+include("QED2dDirac.jl")
+export gamm5Dw, gamm5Dw_sqr, gamm5Dw_sqr_msq, tr_dQwdU
+
+include("QED2dSolver.jl")
+export CG
+
 end # module
--- src/QED2dDirac.jl
+++ src/QED2dDirac.jl
@@ -11,15 +11,15 @@ function gamm5Dw(so, U, si, prm::LattParm)
     id2 = mod1(i2-1, prm.iL[2])
 
     A = 0.5 * ( U[i1,i2,1] * (si[iu1,i2,1] - si[iu1,i2,2]) +
-                conjg(U[id1,i2,1])*(si[id1,i2,1] + si[id1,i2,2]) )
+                conj(U[id1,i2,1])*(si[id1,i2,1] + si[id1,i2,2]) )
     B = 0.5 * ( U[i1,i2,2] * (si[i1,iu2,1] +
                               complex(-imag(si[iu1,i2,2]),real(si[iu1,i2,2]))) +
-                conjg(U[i1,id2,2])*(si[i1,id2,1] +
+                conj(U[i1,id2,2])*(si[i1,id2,1] +
                                     complex(imag(si[iu1,i2,2]),-real(si[iu1,i2,2]))) )
 
     
     so[i1,i2,1] =  2.0 * si[i1,i2,1] + A + B
-    so[i1,i2,2] = -2.0 * si[i1,i2,1] - A - complex(-imag(B),real(B))
+    so[i1,i2,2] = -2.0 * si[i1,i2,2] - A - complex(-imag(B),real(B))
     
 
     return nothing
@@ -62,12 +62,12 @@ function tr_dQwdU(frc, U, X, g5DwX, prm::LattParm)
 
     z1 = conjg(U[i1,i2,1])*( conjg(X[iu1,i2,1])*(g5DwX[i1,i2,1] - g5DwX[i1,i2,2])  -
                            conjg(X[iu1,i2,2])*(g5DwX[i1,i2,1] + g5DwX[i1,i2,2]) )
-    - U[i1,i2,1]*( conjg(X[i1,i2,1])*(g5DwX[iu1,i2,1] - g5DwX[iu1,i2,2])
+    - U[i1,i2,1]*( conjg(X[i1,i2,1])*(g5DwX[iu1,i2,1] - g5DwX[iu1,i2,2]) +
                    conjg(X[i1,i2,2])*(g5DwX[iu1,i2,1] - g5DwX[iu1,i2,2]) )
 
     z2 = conjg(U[i1,i2,2])*( conjg(X[i1,iu2,1])*(g5DwX[i1,i2,1] - (1im)*g5DwX[i1,i2,2])  -
                            conjg(X[i1,iu2,2])*((1im)*g5DwX[i1,i2,1] + g5DwX[i1,i2,2])) 
-    - U[i1,i2,2]*( conjg(X[i1,i2,1])*(g5DwX[i1,iu2,1] + (1im)*g5DwX[i1,iu2,2])
+    - U[i1,i2,2]*( conjg(X[i1,i2,1])*(g5DwX[i1,iu2,1] + (1im)*g5DwX[i1,iu2,2]) +
                    conjg(X[i1,i2,2])*((1im)*g5DwX[i1,iu2,1] - g5DwX[i1,iu2,2]) )
                    
     
