036e6d68b64f2e1801aef2e6621f5bb837e6b96b
dalbandea <albandeadavid@gmail.com>
2021-09-15 14:39:11 +0200

    Fix save_gauge function

--- src/QED2dLoadSave.jl
+++ src/QED2dLoadSave.jl
@@ -1,17 +1,19 @@
 function save_gauge(U, file::String, prm::LattParm)
 	n_links = prm.iL[1]*prm.iL[2]*2
-	Ureal = reshape(real.(U), n_links)
-	Uimag = reshape(imag.(U), n_links)
+	U_h = ones(ComplexF64, prm.iL[1], prm.iL[2], 2)
+	CUDA.copyto!(U_h, U)
+	Ureal = reshape(real.(U_h), n_links)
+	Uimag = reshape(imag.(U_h), n_links)
 	DelimitedFiles.writedlm(file, hcat(Ureal, Uimag))
 	
 	return nothing
 end
 
-function load_gauge(file::String, prm::LattParm)
+function load_gauge(U, file::String, prm::LattParm)
 	tmp = DelimitedFiles.readdlm(file) |> x -> Complex.(x[:, 1], x[:, 2])
 	tmp = reshape(tmp, (prm.iL[1], prm.iL[2], 2))
-	U = CUDA.CuArray{ComplexF64}(undef, (prm.iL[1], prm.iL[2], 2))
+	# U = CUDA.CuArray{ComplexF64}(undef, (prm.iL[1], prm.iL[2], 2))
 	CUDA.copyto!(U, tmp)
 
-	return U
+	return nothing
 end
