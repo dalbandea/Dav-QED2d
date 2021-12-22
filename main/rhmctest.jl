33032d4a0f56af9d527bddb1a300f1931c85ad01
dalbandea <albandeadavid@gmail.com>
2021-12-22 12:24:43 +0100

    Add working RHMC main file and planewave function

--- main/rhmctest.jl
+++ main/rhmctest.jl
@@ -6,7 +6,7 @@ using QED2d
 
 prm  = LattParm((20,20), 6.05)
 kprm = KernelParm((20, 1), (1,20))
-am0 = 0.2
+am0 = 10.0
 read_from = 0
 
 if read_from != 0
@@ -66,20 +66,20 @@ shift = 15 # this shift helps convergence
 
 for i in 1:1000
 	b_aux = copy(b)
-	gamm5Dw_sqr(b_aux, U, b, am0, prm, kprm)
+	# gamm5Dw_sqr(b_aux, U, b, am0, prm, kprm)
 	# gamm5Dw_sqr_sqr(b_aux, U, b, am0, prm, kprm)
 	# CUDA.@cuda threads=kprm.threads blocks=kprm.blocks gamm5Dw(b_aux, U, b, am0, prm)
-	# Dw(b_aux, U, b, am0, prm, kprm)
+	Dw(b_aux, U, b, am0, prm, kprm)
 	# MultiCG(b_aux, U, b, am0, 100000, 0.0000000000000000000001, gamm5Dw_sqr_musq, rprm, prm, kprm)
 	b_aux .= b_aux .+ shift*b
 	global b = b_aux/CUDA.dot(b_aux,b_aux)
 	# println("Segon: $(b[1,1,1])")
 end
 bnext = copy(b)
-gamm5Dw_sqr(bnext, U, b, am0, prm, kprm)
+# gamm5Dw_sqr(bnext, U, b, am0, prm, kprm)
 # gamm5Dw_sqr_sqr(bnext, U, b, am0, prm, kprm)
 # CUDA.@cuda threads=kprm.threads blocks=kprm.blocks gamm5Dw(bnext, U, b, am0, prm)
-# Dw(bnext, U, b, am0, prm, kprm)
+Dw(bnext, U, b, am0, prm, kprm)
 # MultiCG(bnext, U, b, am0, 100000, 0.0000000000000000000001, gamm5Dw_sqr_musq, rprm, prm, kprm)
 bnext .= bnext .+ shift*b
 lambda_max = CUDA.dot(b,bnext)/CUDA.dot(b,b) - shift
@@ -110,11 +110,11 @@ lambda_min = CUDA.dot(b,bnext)/CUDA.dot(b,b) + lambda_max
 
 b = (CUDA.randn(Float64, prm.iL[1], prm.iL[2], 2) .+ CUDA.randn(Float64, prm.iL[1], prm.iL[2], 2)im)/sqrt(2)
 
-CUDA.@cuda threads=kprm.threads blocks=kprm.blocks plwv(b, (2*pi*1/20, 0.0), prm)
+CUDA.@cuda threads=kprm.threads blocks=kprm.blocks plwv(b, (2*pi*0/20, 2*pi*5/20), prm)
 
-CUDA.@cuda threads=kprm.threads blocks=kprm.blocks plwv(b, (0.0, 0.0), prm)
+# CUDA.@cuda threads=kprm.threads blocks=kprm.blocks plwv(b, (0.0, 0.0), prm)
 
 bnext = copy(b)
 Dw(bnext, U, b, am0, prm, kprm)
 lambda = CUDA.dot(b,bnext)/CUDA.dot(b,b)
-bnext/lambda - b
+resres = bnext/lambda - b
